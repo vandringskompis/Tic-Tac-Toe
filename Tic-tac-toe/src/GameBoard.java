68bdad088eb07158d2e7fffd2022fc348ac0e5f6
camillafalk <ccamilla.falk@gmail.com>
2024-10-02 10:56:37 +0200

    Git init. Added Main och classes GameBoard, Players, PlayGame.

---
+++ Tic-tac-toe/src/GameBoard.java
@@ -0,0 +1,11 @@
+public class GameBoard {
+
+
+    public void printGameBoard() {
+        System.out.println("  " + " 0 " + " | " + "0" + " | " + "0" + "\n" + " ---+---+---" + "\n" +
+                "  " + " " + " | " + " " + " | " + "  " + "\n" + "---+----+---" + "\n" +
+                "  " + " | " + "  " + " | " + "  ");
+    }
+
+
+}
---
+++ Tic-tac-toe/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        GameBoard gameBoard = new GameBoard();
+        Players players = new Players();
+
+        System.out.println("\n" + "Welcome to Tic-Tac-Toe" + "\n");
+
+
+        players.addPlayers();
+        gameBoard.printGameBoard();
+        }
+    }
